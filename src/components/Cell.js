24a70012b14ce79c7e284276562d528cf95e2051
Yegor Tovstik <egorzegor@gmail.com>
2019-07-30 15:37:28 +0300

    Make Cell styled

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import styled from "styled-components";
 
-const Cell = (props) => (
-  <div
-    className={`cell ${props.cell.alive ? 'alive' : ''} age-${props.cell.age >= 8 ? 8 : props.cell.age}`}
-    onClick={props.toggle.bind(null, props.cell.x, props.cell.y)} />
+const color = (alive, age) => {
+  if (!alive) { return '#fff'; }
+  switch (age) {
+    case 2:
+      return '#1B76C4';
+    case 3:
+      return '#366EA3';
+    case 4:
+      return '#516682';
+    case 5:
+      return '#6D5E62';
+    case 6:
+      return '#885641';
+    case 7:
+      return '#A34E20';
+    case 8:
+      return '#BF4600';
+    default:
+      return '#007FE5';
+  }
+};
+
+const UnstyledCell = (props) => (
+  <div className={props.className} onClick={props.toggle.bind(null, props.cell.x, props.cell.y)} />
 );
+
+const Cell = styled(UnstyledCell)`
+  display: inline-block;
+  margin: 2px;
+  width: 10px;
+  height: 10px;
+  border: 1px solid #666;
+  border-radius: 10px;
+  background-color: ${props => color(props.cell.alive, props.cell.age)};
+  cursor: pointer;
+`;
 
 export default Cell;
